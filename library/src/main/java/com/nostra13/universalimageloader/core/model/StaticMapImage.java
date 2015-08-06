2d8823ae05b82facb30782ade4bb88ca77160af6
Matt Allen <matt.allen@3sidedcube.com>
2015-08-06 15:20:14 +0100

    Updating the way that markers are added to a location

--- library/src/main/java/com/nostra13/universalimageloader/core/model/StaticMapImage.java
+++ library/src/main/java/com/nostra13/universalimageloader/core/model/StaticMapImage.java
@@ -1,5 +1,11 @@
 package com.nostra13.universalimageloader.core.model;
 
+import android.text.TextUtils;
+
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * // TODO Add class description
  *
@@ -8,7 +14,7 @@ package com.nostra13.universalimageloader.core.model;
  */
 public class StaticMapImage implements ImageServiceOptions
 {
-	private static final String URL = "https://maps.googleapis.com/maps/api/staticmap?center=%s,%s&zoom=%d&size=%dx%d&markers=color:%s";
+	private static final String URL = "https://maps.googleapis.com/maps/api/staticmap?center=%s,%s&zoom=%d&size=%dx%d";
 	public static final String BLACK = "black";
 	public static final String BROWN = "brown";
 	public static final String GREEN = "green";
@@ -22,32 +28,92 @@ public class StaticMapImage implements ImageServiceOptions
 
 	private double latitude, longitude;
 	private int width, height, zoom;
-	private String colour;
+	private List<Marker> markers;
 
-	public StaticMapImage(double latitude, double longitude, int width, int height, int zoom, String colour)
+	public StaticMapImage(double latitude, double longitude, int width, int height, int zoom, List<Marker> markers)
 	{
 		this.latitude = latitude;
 		this.longitude = longitude;
 		this.width = width;
 		this.height = height;
 		this.zoom = zoom;
-		this.colour = colour;
+		this.markers = markers;
+		addMarker(new StaticMapImage.Marker("Gr", 50.724224, -1.906538, StaticMapImage.GREEN));
 	}
 
 	public StaticMapImage(double latitude, double longitude)
 	{
-		this(latitude, longitude, 800, 400, 11, BLUE);
+		this(latitude, longitude, 800, 400, 11, null);
+	}
+
+	public void addMarker(Marker marker)
+	{
+		if (markers == null)
+		{
+			markers = new ArrayList<>();
+		}
+		markers.add(marker);
+	}
+
+	public void setMarkers(List<Marker> markers)
+	{
+		this.markers = markers;
 	}
 
 	@Override
 	public String createUrl()
 	{
-		return String.format(URL, String.valueOf(latitude), String.valueOf(longitude), zoom, width, height, colour);
+		String url = String.format(URL, String.valueOf(latitude), String.valueOf(longitude), zoom, width, height);
+		if (markers != null && markers.size() > 0)
+		{
+			for (Marker marker : markers)
+			{
+				url += marker.createUrl();
+			}
+		}
+		return url;
 	}
 
 	@Override
 	public void fromUrl(String url)
 	{
 		// Not needed
+	}
+
+	public static class Marker implements ImageServiceOptions
+	{
+		private static final String URL_LABEL = "%7Clabel:";
+		private double latitude, longitude;
+		private String colour, label;
+
+		public Marker(String label, double latitude, double longitude, String colour)
+		{
+			this.label = label;
+			this.latitude = latitude;
+			this.longitude = longitude;
+			this.colour = colour;
+		}
+
+		public Marker(double latitude, double longitude, String colour)
+		{
+			this(null, latitude, longitude, colour);
+		}
+
+		@Override
+		public String createUrl()
+		{
+			String base = "&markers=color:" + colour + "%7C" + String.valueOf(latitude) + "," + String.valueOf(longitude);
+			if (!TextUtils.isEmpty(label))
+			{
+				base += URL_LABEL + label;
+			}
+			return base;
+		}
+
+		@Override
+		public void fromUrl(String url)
+		{
+			// Not needed
+		}
 	}
 }
