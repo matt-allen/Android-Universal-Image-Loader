2ead1d19771c2ec1a0238618d6196f9df244e2ea
Matt Allen <mattallen092@gmail.com>
2015-08-20 21:27:09 +0100

    Updated data sets

--- sample/src/main/java/com/nostra13/universalimageloader/sample/fragment/fallback/FallbackImageServiceFragment.java
+++ sample/src/main/java/com/nostra13/universalimageloader/sample/fragment/fallback/FallbackImageServiceFragment.java
@@ -2,6 +2,7 @@ package com.nostra13.universalimageloader.sample.fragment.fallback;
 
 import com.nostra13.universalimageloader.image.GravatarImage;
 import com.nostra13.universalimageloader.image.ImageServiceOptions;
+import com.nostra13.universalimageloader.image.SimpleTextImage;
 import com.nostra13.universalimageloader.image.StaticMapImage;
 import com.nostra13.universalimageloader.sample.recyclerview.BaseAdapter;
 import com.nostra13.universalimageloader.sample.recyclerview.ServiceOptionsAdapter;
@@ -27,7 +28,16 @@ public class FallbackImageServiceFragment extends BaseFallbackImageFragment<Imag
 	{
 		List<ImageServiceOptions> data = new ArrayList<>();
 		data.add(new GravatarImage("matt.allen@3sidedcube.com"));
-		data.add(new StaticMapImage(50.29384, -1.92367));
+		data.add(new StaticMapImage(51.101797, -1.219482));
+		data.add(new StaticMapImage(51.813709, -1.285400));
+		data.add(new SimpleTextImage("Tom Jones", SimpleTextImage.COLOUR_DEEP_ORANGE));
+		data.add(new StaticMapImage.Builder()
+				.latitude(51.813709)
+				.longitude(-1.285400)
+				.addMarker(new StaticMapImage.Marker("One", 51.813709, -1.285405, StaticMapImage.GREEN))
+				.defaultMarker(true)
+				.build());
+		data.add(new GravatarImage("callum@3sidedcube.com"));
 		return data;
 	}
 }
--- sample/src/main/java/com/nostra13/universalimageloader/sample/fragment/fallback/FallbackStringArrayFragment.java
+++ sample/src/main/java/com/nostra13/universalimageloader/sample/fragment/fallback/FallbackStringArrayFragment.java
@@ -13,7 +13,6 @@ import java.util.List;
  */
 public class FallbackStringArrayFragment extends BaseFallbackImageFragment<String[]>
 {
-
 	@Override
 	protected BaseAdapter<String[]> getAdapter()
 	{
@@ -24,7 +23,33 @@ public class FallbackStringArrayFragment extends BaseFallbackImageFragment<Strin
 	protected List<String[]> getDataSet()
 	{
 		List<String[]> strings = new ArrayList<>();
-		strings.add(new String[]{"https://lh6.googleusercontent.com/-55osAWw3x0Q/URquUtcFr5I/AAAAAAAAAbs/rWlj1RUKrYI/s1024/A%252520Photographer.jpg"});
+		strings.add(new String[]{
+				"https://lh6.googleusercontent.com/-55osAWw3x0Q/URquUtcFr5I/AAAAAAAAAbs/rWlj1RUKrYI/s1024/A%252520Photographer.jpg",
+				"https://lh3.googleusercontent.com/-rrFnVC8xQEg/URqufdrLBaI/AAAAAAAAAbs/s69WYy_fl1E/s1024/Chess.jpg"
+		});
+		strings.add(new String[]{
+				"https://lh3.googleusercontent.com/-R3_EYAyJvfk/URquzQBv8eI/AAAAAAAAAbs/b9xhpUM3pEI/s1024/Light%252520to%252520Sand.jpg",
+				"https://lh3.googleusercontent.com/-fHY5h67QPi0/URqu0Cp4J1I/AAAAAAAAAbs/0lG6m94Z6vM/s1024/Little%252520Bit%252520of%252520Paradise.jpg",
+				"https://lh5.googleusercontent.com/-TzF_LwrCnRM/URqu0RddPOI/AAAAAAAAAbs/gaj2dLiuX0s/s1024/Lone%252520Pine%252520Sunset.jpg"
+		});
+		strings.add(new String[]{
+				"https://lh6.googleusercontent.com/-83oOvMb4OZs/URqvJL0T7lI/AAAAAAAAAbs/c5TECZ6RONM/s1024/Tokyo%252520Smog.jpg",
+				"https://lh3.googleusercontent.com/-FB-jfgREEfI/URqvJI3EXAI/AAAAAAAAAbs/XfyweiRF4v8/s1024/Tufa%252520at%252520Night.jpg"
+		});
+		strings.add(new String[]{
+				"https://google.com/thiswontworkm8",
+				"https://lh6.googleusercontent.com/-0CXJRdJaqvc/URqu1opNZNI/AAAAAAAAAbs/PFB2oPUU7Lk/s1024/Look%252520Me%252520in%252520the%252520Eye.jpg",
+				"https://lh3.googleusercontent.com/-D_5lNxnDN6g/URqu2Tk7HVI/AAAAAAAAAbs/p0ddca9W__Y/s1024/Lost%252520in%252520a%252520Field.jpg"
+		});
+		strings.add(new String[]{
+				"https://lh3.googleusercontent.com/-4HdpJ4_DXU4/URqu046dJ9I/AAAAAAAAAbs/eBOodtk2_uk/s1024/Lonely%252520Rock.jpg",
+				"https://lh6.googleusercontent.com/-erbF--z-W4s/URqu1ajSLkI/AAAAAAAAAbs/xjDCDO1INzM/s1024/Longue%252520Vue.jpg"
+		});
+		strings.add(new String[]{
+				"https://lh5.googleusercontent.com/-WVpRptWH8Yw/URqugh-QmDI/AAAAAAAAAbs/E-MgBgtlUWU/s1024/Chihuly.jpg",
+				"https://lh5.googleusercontent.com/-0BDXkYmckbo/URquhKFW84I/AAAAAAAAAbs/ogQtHCTk2JQ/s1024/Closed%252520Door.jpg",
+				"https://lh3.googleusercontent.com/-PyggXXZRykM/URquh-kVvoI/AAAAAAAAAbs/hFtDwhtrHHQ/s1024/Colorado%252520River%252520Sunset.jpg"
+		});
 		return strings;
 	}
 }
