1e8ceb075e6d038de593223dc2876de3ed669341
Matt Allen <matt.allen@3sidedcube.com>
2015-09-15 14:59:43 +0100

    Added more unit tests

--- library/src/test/java/com/nostra13/universalimageloader/core/download/ExtensibleImageDownloadTest.java
+++ library/src/test/java/com/nostra13/universalimageloader/core/download/ExtensibleImageDownloadTest.java
@@ -6,6 +6,7 @@ import com.nostra13.universalimageloader.core.helper.FlickrServiceHelper;
 import com.nostra13.universalimageloader.image.FlickrImage;
 import com.nostra13.universalimageloader.image.GravatarImage;
 import com.nostra13.universalimageloader.image.SimpleTextImage;
+import com.nostra13.universalimageloader.image.StaticMapImage;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -15,6 +16,8 @@ import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import static junit.framework.Assert.assertTrue;
 import static junit.framework.TestCase.assertNotNull;
@@ -60,12 +63,50 @@ public class ExtensibleImageDownloadTest
 	}
 
 	@Test
+	public void testFlickrImageDownloadFail()
+	{
+		try
+		{
+			ExtensibleImageDownloader downloader = new ExtensibleImageDownloader(activity);
+			FlickrServiceHelper.setApiKey("ff09527c98fd10a0fd81e62986cbbc8d");
+			FlickrImage image = new FlickrImage(606660, 222220);
+			System.out.println("Getting image for address: " + image.createUrl());
+			InputStream stream = downloader.getStream(image.createUrl(), null);
+			assertNull(stream);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	@Test
 	public void testSimpleTextImageCreation()
 	{
 		try
 		{
 			ExtensibleImageDownloader downloader = new ExtensibleImageDownloader(activity);
 			SimpleTextImage image = new SimpleTextImage("Sample Name");
+			System.out.println("Getting image for address: " + image.createUrl());
+			InputStream stream = downloader.getStream(image.createUrl(), null);
+			assertNotNull(stream);
+			assertTrue(stream.read() >= 0);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	@Test
+	public void testSimpleTextImageCreationFail()
+	{
+		try
+		{
+			ExtensibleImageDownloader downloader = new ExtensibleImageDownloader(activity);
+			SimpleTextImage image = new SimpleTextImage(null);
 			System.out.println("Getting image for address: "+image.createUrl());
 			InputStream stream = downloader.getStream(image.createUrl(), null);
 			assertNotNull(stream);
@@ -104,9 +145,43 @@ public class ExtensibleImageDownloadTest
 		{
 			ExtensibleImageDownloader downloader = new ExtensibleImageDownloader(activity);
 			GravatarImage image = new GravatarImage("mattallen092@thiswontwork.com");
-			System.out.println("Getting image for address: "+image.createUrl());
+			System.out.println("Getting image for address: " + image.createUrl());
 			InputStream stream = downloader.getStream(image.createUrl(), null);
 			assertNull(stream);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	@Test
+	public void testStaticMapImageDownload()
+	{
+		try
+		{
+			ExtensibleImageDownloader downloader = new ExtensibleImageDownloader(activity);
+			List<StaticMapImage.Marker> markers = new ArrayList<>();
+			markers.add(new StaticMapImage.Marker("A", 50.724097, -1.906289, StaticMapImage.RED));
+			markers.add(new StaticMapImage.Marker("A", 50.724097, -1.906289, "notacolour"));
+			markers.add(new StaticMapImage.Marker(null, 50724097, -1.906289, StaticMapImage.RED));
+			markers.add(new StaticMapImage.Marker("A", 50.724097, -1906289, StaticMapImage.RED));
+			markers.add(new StaticMapImage.Marker("A", 50.724097, -1.906289, null));
+			markers.add(null);
+			for (StaticMapImage.Marker marker : markers)
+			{
+				StaticMapImage image = new StaticMapImage.Builder()
+						.defaultMarker(true)
+						.latitude(50.724057)
+						.longitude(-1.906289)
+						.addMarker(marker)
+						.build();
+				System.out.println("Getting image for address: " + image.createUrl());
+				InputStream stream = downloader.getStream(image.createUrl(), null);
+				assertNotNull(stream);
+				assertTrue(stream.read() >= 0);
+			}
 		}
 		catch (Exception e)
 		{
